c79ac6a713083bb435ea1fbd2d212001f8bf08f0
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-02 15:29:44 +0545

    Create nCr.java

---
+++ Functions/nCr.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class nCr{
+    public static int factorial(int n){ // find factorial
+        int fact = 1;
+        for(int i = 1; i <= n; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        System.out.print("Enter r : ");
+        int r = sc.nextInt();
+        //for calculating nCr
+        int ncr = factorial(n) / (factorial(r) * factorial(n-r));
+      
+        System.out.println("nCr : " + ncr);
+    }
+}
