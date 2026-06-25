44d1343d4cc9f5b276c30c6abe73e27dbe000825
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-25 13:31:46 +0545

    Add ReadCompleteFile.java to read a complete file

---
+++ File Handling/File Reading/ReadCompleteFile.java
@@ -0,0 +1,16 @@
+import java.io.FileReader;
+
+public class ReadCompleteFile {
+    public static void main(String args[]){
+        try{
+            FileReader fr = new FileReader("java.txt");
+            int ch;
+            while((ch = fr.read()) != -1){
+                System.out.print((char) ch);
+            }
+            fr.close();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
